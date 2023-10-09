4e846926feb532a85c6b59affe13c18a6980b63e
Michal Hájek <hajek.michal55@gmail.com>
2023-10-09 09:55:16 +0200

    fix: add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-   root "posts#index"
+  get "/departments", to: "departments#index"
+
+  get ":department_id/souvenirs", to: "souvenirs#index", as: :objects_path
+  get "/about", to: "museums#about"
+  root "museums#about"
 end
