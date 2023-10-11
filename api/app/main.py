2d0b6537c79d7c615a2ed335fa0d5037d53c6a17
Rastislav Tvarožek <TvarozekRastislav>
2023-10-11 09:53:47 +0200

    feat: fast api db integration, networking

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
-
+import psycopg2
+import os
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    host = os.getenv("POSTGRES_HOST")
+    db = os.getenv("POSTGRES_DB")
+    user = os.getenv("POSTGRES_USER")
+    pswd = os.getenv("POSTGRES_PASSWORD")
+
+    conn = psycopg2.connect(host=host, dbname=db, user=user, password=pswd, port=5432)
+    cur = conn.cursor()
+    cur.execute("select current_date;")
+
+    return {"return": cur.fetchone()}
