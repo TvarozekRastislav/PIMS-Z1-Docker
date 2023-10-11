2e0f878c8c8167492d252d6051778030d1c644f0
Rastislav Tvarožek <TvarozekRastislav>
2023-10-11 11:38:15 +0200

    feat: api, endpoits, db

--- api/app/main.py
+++ api/app/main.py
@@ -1,18 +1,67 @@
-from fastapi import FastAPI
-import psycopg2
-import os
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+import crud_handler
+import math
+
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    host = os.getenv("POSTGRES_HOST")
-    db = os.getenv("POSTGRES_DB")
-    user = os.getenv("POSTGRES_USER")
-    pswd = os.getenv("POSTGRES_PASSWORD")
+def all_records():
+    data = crud_handler.get_all()
+    if data.empty:
+        raise HTTPException(status_code=404, detail="No data")
+    return data.to_dict(orient="records")
 
-    conn = psycopg2.connect(host=host, dbname=db, user=user, password=pswd, port=5432)
-    cur = conn.cursor()
-    cur.execute("select current_date;")
 
-    return {"return": cur.fetchone()}
+@app.get("/{throw_id}")
+def id_record(throw_id: str):
+    data = crud_handler.get_throw_id(throw_id)
+    if data.empty:
+        raise HTTPException(status_code=404, detail="No record with given id")
+    return data.to_dict(orient="records")
+
+
+@app.delete("/{throw_id}")
+def remove_thorw(throw_id: str):
+    data = crud_handler.remove_throw(throw_id)
+    return data.to_dict(orient="records")
+
+
+@app.post("/")
+async def root(request: Request):
+    G = 9.81
+    TS = 0.01
+    fall_data = []
+
+    try:
+        request = await request.json()
+        speed = request["speed"]
+        angle = request["angle"]
+
+        if speed < 0 or angle < 0:
+            raise HTTPException(status_code=400, detail="Input cannot be negative")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Wrong input")
+
+    x, y, t = 0.0, 0.0, 0.0
+    ANGLE_RAD = math.radians(angle)
+    FALL_TIME = (2 * speed * math.sin(ANGLE_RAD)) / G
+
+    fall_data.append({"t": 0.0, "x": 0.0, "y": 0.0})
+
+    while t < FALL_TIME:
+        t += TS
+        x = speed * t * math.cos(ANGLE_RAD)
+        y = speed * t * math.sin(ANGLE_RAD) - (G * math.pow(t, 2) / 2.0)
+
+        if t > FALL_TIME:
+            x = (math.pow(speed, 2) * math.sin(2.0 * ANGLE_RAD)) / G
+            fall_data.append({"x": x, "y": 0.0, "t": FALL_TIME})
+
+            return JSONResponse(crud_handler.insert_calc(fall_data))
+
+        fall_data.append({"x": x, "y": y, "t": t})
+
+    raise RuntimeError("ERROR: Logical error")
